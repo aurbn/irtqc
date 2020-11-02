5ca1838f4144bac0d15acdf09a46ba71a594c6be
aurbn <anatoly.urban@gmail.com>
2020-11-02 03:09:44 +0300

    Classify 2

--- main.py
+++ main.py
@@ -1,111 +1,24 @@
 from argparse import ArgumentParser
 
-from pyteomics import mzml, mgf, tandem
-from functools import partial
+from pyteomics import mzml
 import numpy as np
 import pandas as pd
-from collections import defaultdict
 from matplotlib import pyplot as plt
-from pprint import pprint
 import tqdm
 import scipy.signal as sgn
 from scipy.ndimage.filters import gaussian_filter1d
-import bokeh.plotting as bk
 import itertools
 
 from scipy.special import erf
 from scipy.optimize import curve_fit
 
 import sys
-#import warnings; warnings.simplefilter('ignore')
 
-mdelta = 1e-4
-ms2_window_ppm = 0.001*1e6
-eic_ppm = 2.5
-
-
-
-#eic_width_unit	ppm
-eic_width_value = 5.0
-width_1_pc = 0.50
-width_2_pc = 0.05
-
-
-
-ms1_peak_apexs = []
-ms1_xic_ints =[]
-ms1_fit_area = []
-ms1_fit_width = []
-
-#time boundaries
-ms1_pc_w1_left = []
-ms1_pc_w1_right = []
-ms1_pc_w2_left = []
-ms1_pc_w2_right = []
-
-#index boundaries
-ms1_pc_w1_left_i = []
-ms1_pc_w1_right_i = []
-ms1_pc_w2_left_i = []
-ms1_pc_w2_right_i = []
-
-ms1_xic_area_w1 = []
-ms1_xic_area_w2 = []
-
-
-def asym_peak(t, *pars):
-    #'from Anal. Chem. 1994, 66, 1294-1301'
-    a0 = pars[0]  # peak area
-    a1 = pars[1]  # elution time
-    a2 = pars[2]  # width of gaussian
-    a3 = pars[3]  # exponential damping term
-    f = (a0 / 2 / a3 * np.exp(a2 ** 2 / 2.0 / a3 ** 2 + (a1 - t) / a3)
-         * (erf((t - a1) / (np.sqrt(2.0) * a2) - a2 / np.sqrt(2.0) / a3) + 1.0))
-    return f
-
-
-def mq(m1, m2, delta):
-    return abs(m1 - m2) < delta
-
-
-#def mw(m1, m2):
-#    return abs(m1 - m2) < mwindow
-
-
-def me(m1, m2, ppm):
-    return abs(m1 - m2) < eic_ppm * 1e-6 * (m1 + m2) / 2
-
-
-def get_prec_mz(scan):
-    assert scan['ms level'] == 2, "Ms level must be == 2"
-
-
-
-# def get_mz_intensity(scan, mz, ppm):
-#     mza, inta = get_spectrum(scan)
-#     left = mza.searchsorted(mz * (1 - eic_ppm * 1e-6))
-#     right = mza.searchsorted(mz * (1 + eic_ppm * 1e-6))
-#     return inta[left:right].sum()
-
-
-def get_xic_apex(xic, times=None):
-    peaksi = sgn.find_peaks(xic, threshold=np.mean(xic))[0]
-    peaksamp = xic[peaksi]
-    maxpeaki = np.argmax(peaksamp)
-    apexi = peaksi[maxpeaki]
-    # print(peaksi)
-    return apexi, xic[apexi], times[apexi] if len(times) else None
-
-
-def dict_npize(d):
-    return {k: np.array(v) for k, v in d.items()}
-
-
-def precmz_to_float(mz, listfloat):
-    for f in listfloat:
-        if me(mz, f, eic_ppm):
-            return f
-    assert False, "No mass in list"
+# def precmz_to_float(mz, listfloat):
+#     for f in listfloat:
+#         if me(mz, f, eic_ppm):
+#             return f
+#     assert False, "No mass in list"
 
 class WrongMSLevel(Exception):
     """Wrong MS level"""
@@ -117,14 +30,128 @@ class WrongMSLevel(Exception):
 
     def __str__(self):
         if self.__given:
-            if type(self) == int:
+            if isinstance(self.__given, int):
                 return f'Expected MS level {self.__expected}, given {self.__given}'
-            elif type(self.__given) == Scan:
+            elif isinstance(self.__given, Scan):
                 return f'Expected MS level {self.__expected}, given {self.__given.mslevel}'
             else:
-                raise ValueError
+                raise TypeError
         else:
             return f'Wrong MS level, should be {self.__expected}'
+
+class T:
+    def __init__(self, time):
+        self.__time  = time
+
+    def __str__(self):
+        return f'{self.__time} min'
+
+    def __int__(self):
+        return self.__time
+
+class Chromatogram:
+    """Simple chromatogram"""
+
+    def __init__(self, times, ints):
+        """Constructor for Chromatogram"""
+        self.__times = times
+        self.__ints = ints
+
+    @property
+    def t(self):
+        """Returns internal times array"""
+        return self.__times
+
+    @property
+    def i(self):
+        """Returns internal intensity array"""
+        return self.__ints
+
+    def plot(self, ax, *args, **kwargs):
+        ax.plot(self.__times, self.__ints, *args, **kwargs)
+
+    def get_apex(self):
+        peaksi, _ = sgn.find_peaks(self.__ints, threshold=self.__ints.mean())
+        assert len(peaksi), "No peaks found"
+        peaksamp = self.__ints[peaksi]
+        maxpeaki = np.argmax(peaksamp)
+        apexi = peaksi[maxpeaki]
+        return apexi, self.__ints[apexi], self.__times[apexi]
+
+    def get_apex_time(self):
+        return self.get_apex()[2]
+
+    def get_apex_int(self):
+        return self.get_apex()[1]
+
+    def _get_width_indexs(self, apex_pc):
+        apexa = self.get_apex_int()
+        left = np.where(self.__ints>apexa*apex_pc/100)[0][0]
+        right = np.where(self.__ints>apexa*apex_pc/100)[0][-1]
+        return left, right
+
+    def get_width(self, apex_pc):
+        left, right = self._get_width_indexs(apex_pc)
+        return self.__times[left], self.__times[right]
+
+    def get_width_pc_area(self, apex_pc):
+        left, right = self._get_width_indexs(apex_pc)
+        times = self.__times[left:right]
+        times_sh = self.__times[left+1:right+1]
+        deltas = times_sh - times
+        ints = self.__ints[left:right]
+        return sum(deltas * ints)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            if item.step is None:
+                left = np.searchsorted(self.__times, item.start)
+                right = np.searchsorted(self.__times, item.stop)
+                return Chromatogram(self.__times[left:right], self.__ints[left:right])
+            else:
+                raise NotImplementedError
+        else:
+            index = np.searchsorted(self.__times, item)
+            return self.__ints[index]
+
+    def smooth(self, type='gaussian', **kwargs):
+        if type == 'gaussian':
+            return Chromatogram(self.__times, gaussian_filter1d(self.__ints, **kwargs))
+        else:
+            raise NotImplementedError('Only gaussian smoothing is supported')
+
+
+class Spectrum:
+    """Simgle spectrum"""
+
+    def __init__(self, mza, inta, mslevel=None, precursor=None, time=None):
+        """Constructor for Spectrum"""
+
+        self.__mza = mza
+        self.__inta = inta
+        self.__level = mslevel
+        self.__prec = precursor
+        self.__time = time
+
+    @property
+    def mz(self):
+        return self.__mza
+
+    @property
+    def i(self):
+        return self.__inta
+
+    def __getitem__(self, item):
+        if isinstance(item, float):
+            index = np.searchsorted(self.__mza, item)
+            return self.__inta[index]
+        elif isinstance(item, slice):
+            left = np.searchsorted(self.__mza, item.start)
+            right = np.searchsorted(self.__mza, item.stop)
+            return Spectrum(self.__mza[left:right], self.__inta[left:right],
+                            self.__level, self.__prec, self.__time)
+        else:
+            raise TypeError
 
 
 class Scan:
@@ -133,27 +160,27 @@ class Scan:
         self.__s = scan
 
     @property
-    def time(self):
+    def time(self) -> float:
         return self.__s['scanList']['scan'][0]['scan start time']
 
     @property
-    def tic(self):
+    def tic(self) -> float:
         return self.__s['total ion current']
 
     @property
-    def mzarray(self):
+    def mzarray(self) -> np.ndarray:
         return self.__s['m/z array']
 
     @property
-    def intarray(self):
+    def intarray(self) -> np.ndarray:
         return self.__s['intensity array']
 
     @property
-    def mslevel(self):
+    def mslevel(self) -> int:
         return self.__s['ms level']
 
     @property
-    def precursor(self):
+    def precursor(self) -> float:
         if self.mslevel == 2:
             return self.__s['precursorList']['precursor'][0]\
                 ['selectedIonList']['selectedIon'][0]['selected ion m/z']
@@ -161,34 +188,26 @@ class Scan:
             raise WrongMSLevel(2, self)
 
 
-class Tic:
-    """Class for TIC chromatogram"""
-
-    def __init__(self,):
-        self.__times = []
-        self.__ints = []
-
-    def append(self, scan: Scan):
-        self.__times.append(scan.time)
-        self.__ints.append(scan.tic)
-
-
-class MSn:
+class MSnScans:
     """Class for storing MSn data for single MS level"""
 
     def __init__(self, mslevel):
         """Constructor for Msn"""
         self.__mslevel = mslevel
-        self.__times =[]
+        self.__tic = []
+        self.__times = []
         self.__mza = []
         self.__inta = []
         if mslevel == 2:
             self.__precs = []
-        else:
+        elif mslevel == 1:
             self.__precs = None
+        else:
+            raise WrongMSLevel(0, 2) #Ewww...
 
     def append(self, scan: Scan) -> bool:
         if scan.mslevel == self.__mslevel:
+            self.__tic.append(scan.tic)
             self.__times.append(scan.time)
             self.__mza.append(scan.mzarray)
             self.__inta.append(scan.intarray)
@@ -197,36 +216,56 @@ class MSn:
             return True #Appended
         return False #Skip
 
+    def finish(self):
+        """Should be called after all values are added"""
 
-class MsExperiment:
-    """Class for single MSMS experiment"""
+        self.__times = np.array(self.__times)
+        self.__tic = np.array(self.__tic)
+        if self.__mslevel == 2:
+            self.__precs = np.array(self.__precs)
+
+    @property
+    def tic(self):
+        return Chromatogram(self.__times, self.__tic)
+
+    def xic(self, mz, ppm):
+        if self.__mslevel != 1:
+            raise WrongMSLevel(1)
+        res = []
+        for mza, inta in zip(self.__mza, self.__inta):
+            left = mza.searchsorted(mz * (1 - ppm * 1e-6))
+            right = mza.searchsorted(mz * (1 + ppm * 1e-6))
+            res.append(inta[left:right].sum())
+
+        res = np.array(res)
+        assert len(res) == len(self.__times), "Wrong length of chromatogram"
+        return Chromatogram(self.__times, res)
+
+    def __getitem__(self, item):
+        if isinstance(item, float):
+            index = np.searchsorted(self.__times, item)
+            return Spectrum(self.__mza[index], self.__inta[index], self.__mslevel,
+                            self.__precs[index] if self.__precs else None,
+                            self.__times[index])
+        else:
+            raise NotImplementedError
+
+
+class LCMSMSExperiment:
+    """Class for single LCMSMS experiment"""
 
     def __init__(self, mzmlsource):
-        self.tic = Tic()
-        self.ms1 = MSn(mslevel=1)
-        self.ms2 = MSn(mslevel=2)
+        self.ms1 = MSnScans(mslevel=1)
+        self.ms2 = MSnScans(mslevel=2)
 
         for s_ in mzmlsource:
             scan = Scan(s_)
-            self.tic.append(scan)
             self.ms1.append(scan)
             self.ms2.append(scan)
 
+        self.ms1.finish()
+        self.ms2.finish()
 
-            #     for t in targets_list:
-            #         xics[t].append(get_mz_intensity(scan, t, eic_ppm))
-            #
-            # if scan['ms level'] == 2:
-            #     # import ipdb; ipdb.set_trace() # BREAKPOINT
-            #     prec = get_prec_mz(scan)
-            #     prec = precmz_to_float(prec, targets_frags.keys())
-            #     for frag in targets_frags[prec]:
-            #         ms2_xics[(prec, frag)].append(get_mz_intensity(scan, frag, ms2_window_ppm))
-            #         ms2_xictimes[(prec, frag)].append(scantime(scan))
-            #
-            #     #  ms2_scans_mz[(prec,frag)].append(scan['m/z array'])
-            #     #  ms2_scans_int[(prec,frag)].append(scan['intensity array'])
-            #     #  ms2_scans_time[(prec,frag)].append(scantime(scan))
 
 
 if __name__ == '__main__':
@@ -239,9 +278,16 @@ if __name__ == '__main__':
     argparser = argparser.parse_args()
 
 
-    numscans = 0
-    exp = MsExperiment(tqdm.tqdm(mzml.MzML(argparser.mzml)))
+    exp = LCMSMSExperiment(tqdm.tqdm(mzml.MzML(argparser.mzml)))
 
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    exp.ms1.tic.plot(ax, "Ms1", "g-")
+    fig.savefig("tic.png")
+
+    targets = pd.read_csv("./iRT2/qc1_targets.csv", sep='\t')
+
+    pass
 
 
 
