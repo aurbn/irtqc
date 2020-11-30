12c61c7b70e223c25791d86185763b3e697a628b
aurbn <anatoly.urban@gmail.com>
2020-11-30 16:57:27 +0300

    Added lxml dependency.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/aurbn/irtqc",
     packages=setuptools.find_packages(),
-    install_requires=['pyteomics[DF]', 'matplotlib', 'pandas', 'tqdm', 'numpy', 'scipy'],
+    install_requires=['pyteomics[DF]', 'lxml', 'matplotlib', 'pandas', 'tqdm', 'numpy', 'scipy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
